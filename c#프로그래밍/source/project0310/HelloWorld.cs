ec623d2bbcd0ee468d5fffa693335e0fb9f57b92
dongcheolpark <head02325@gmail.com>
2021-03-17 16:17:30 +0900

    a

---
+++ c#프로그래밍/source/project0310/HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace project0310
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello,world!");
+        }
+    }
+}
