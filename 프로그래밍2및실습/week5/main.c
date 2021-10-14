7315dceceef6483fb446119710e99383d3663f2a
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2021-10-14 09:54:31 +0900

    a

--- 프로그래밍2및실습/week5/main.c
+++ 프로그래밍2및실습/week5/main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(void)
 {
+	char * str[10];
+	int n = 10;
+	for(int i = 0;i<n;i++) {
+		char tmp[21];
+		scanf("%s",tmp);
+		str[i] = (char *)malloc(sizeof(char)*strlen(tmp));
+		strcpy(str[i],tmp);
+	}
+	for(int i = 0;i<n;i++) {
+		printf("%s\n",str[i]);
+		free(str[i]);
+	}
 	return 0;
 }
