813ed614df318a601395166fd88033666f8d7368
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2021-11-28 18:17:30 +0900

    a

--- 프로그래밍2및실습/week13/a.c
+++ 프로그래밍2및실습/week13/a.c
@@ -1,69 +1,82 @@
 #include <stdio.h>
-#include <string.h>
+
 #include <stdlib.h>
+
+#include <string.h>
 
 #define N 20
 
-struct linked_list
-{
-	char d;
-	struct linked_list *next;
+struct linked_list {
+
+char d;
+
+struct linked_list *next;
+
 };
 
 typedef struct linked_list ELEMENT;
 
 typedef ELEMENT *LINK;
 
-LINK string_to_list(char s[])
-{
-	LINK head,prev,node;
-	if (s[0] == '\0')
-		return NULL;
-	else {
-		for(int i = 0;s[i] != '\0';i++) {
-			node = malloc(sizeof(ELEMENT));
-			if(i == 0) 
-				head = node;
-			else 
-				prev->next = node;
-			node->d = s[i];
-			node->next = NULL;
-			prev = node;
-		}
-	}
+LINK string_to_list(char s[]) {
+	 LINK head=NULL, node, last;
+    int i = 0;
+    for(int i=0; i<strlen(s); i++){
+        if(s[i]=='\0')  break;
+        else{
+            node = malloc(sizeof(ELEMENT));
+            node -> d = s[i];
+            node -> next = NULL;
+        }
+        if(head=NULL)   head = node;
+        else last -> next = node;
+        last = node;
+    }
 	return head;
 }
-void free_linked_list(LINK h) {
-	if(h == NULL) return;
-	free_linked_list(h->next);
-	free(h);
-}
-void print_list(LINK head)
-{
-	if (head == NULL)
-		printf("NULL \n");
-	else
-	{
-		printf("%c --> ", head->d);
-		print_list(head->next);
-	}
+
+void print_list(LINK head) {
+
+if (head == NULL)
+
+printf("NULL \n");
+
+else {
+
+printf("%c --> ", head -> d);
+
+print_list(head -> next);
+
 }
 
-int main(void)
-{
-	char input[N];
-	while(1) {
-		LINK h;
-		printf("문자열 입력: ");
-		scanf("%s", input);
-		if(strcmp(input,"exit") == 0) {
-			puts("BYE......");
-			break;
-		}
-		h = string_to_list(input);
-		printf("변환 리스트 결과 : \n");
-		print_list(h);
-		free_linked_list(h);
-	}
-	return 0;
+}
+
+int main(void) {
+
+char input[N];
+
+LINK h;
+
+int i=1;
+while(i){
+printf("문자열 입력: ");
+
+scanf("%s", input);
+
+if(strcmp(input, "exit")==0){
+    printf("BYE......\n");
+    i=0;
+}
+
+else{
+h = string_to_list(input);
+
+printf("변환 리스트 결과 : \n");
+
+print_list(h);
+}
+
+}
+
+return 0;
 }
